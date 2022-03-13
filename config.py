21f222a89ccb0936095dc0fddcdacee47eff01d8
Stefomat <41241946+Stefomat@users.noreply.github.com>
2022-03-13 14:41:00 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+root_path = os.environ['ROOT_PATH']
+username = os.environ['TWITCH_USERNAME']
+client_id = os.environ['TWITCH_CLIENT_ID']
+client_secret = os.environ['TWITCH_CLIENT_SECRET']
