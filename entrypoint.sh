ebc4350e15243b9f1f5707b07fbac7b4bd81179f
Stefomat <41241946+Stefomat@users.noreply.github.com>
2023-01-07 14:59:54 +0100

    Test for presence of needed env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# Ensure presence of all needed variables
+if [[ -z $USERNAME ]]; then
+  echo "USERNAME not given, exit."
+  exit 1
+fi
+
+if [[ -z $CLIENT_ID ]]; then
+  echo "CLIENT_ID not given, exit."
+  exit 1
+fi
+
+if [[ -z $CLIENT_SECRET ]]; then
+  echo "CLIENT_SECRET not given, exit."
+  exit 1
+fi
+
 # Write config.py config file from Docker env variables
 cat <<EOF > /opt/config.py
 root_path = "/opt/recordings"
