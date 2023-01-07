ad41e952e20a314b302ddd3f70a17c8e00dfc5aa
Stefomat <41241946+Stefomat@users.noreply.github.com>
2023-01-07 14:44:52 +0100

    Hardcode output folder in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Write config.py config file from Docker env variables
 cat <<EOF > /opt/config.py
-root_path = "$ROOT_PATH"
+root_path = "/opt/recordings"
 username = "$USERNAME"
 client_id = "$CLIENT_ID"
 client_secret = "$CLIENT_SECRET"
