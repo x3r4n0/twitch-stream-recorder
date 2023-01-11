f2d67a9ebaafcda1f796507fe15fbc1f4a4db183
Stefomat <41241946+Stefomat@users.noreply.github.com>
2023-01-11 19:48:18 +0100

    Don't use -z option

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Ensure presence of all needed variables
-[ -z "$USERNAME" ] || echo "USERNAME not given, exit." && exit 1
-[ -z "$CLIENT_ID" ] || echo "CLIENT_ID not given, exit." && exit 1
-[ -z "$CLIENT_SECRET" ] || echo "CLIENT_SECRET not given, exit." && exit 1
+[ "$USERNAME" ] || echo "USERNAME not given, exit." && exit 1
+[ "$CLIENT_ID" ] || echo "CLIENT_ID not given, exit." && exit 1
+[ "$CLIENT_SECRET" ] || echo "CLIENT_SECRET not given, exit." && exit 1
 
 # Write config.py config file from Docker env variables
 cat <<EOF > /opt/config.py
