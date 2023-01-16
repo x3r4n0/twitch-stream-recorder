06a427d14204b571a1952891868fbfffd597b159
Stefomat <41241946+Stefomat@users.noreply.github.com>
2023-01-16 15:08:53 +0100

    Put braces around variable references

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Ensure presence of all needed variables
-[ "$USERNAME" ] || { echo "USERNAME not given, exit."; exit 1; }
-[ "$CLIENT_ID" ] || { echo "CLIENT_ID not given, exit."; exit 1; }
-[ "$CLIENT_SECRET" ] || { echo "CLIENT_SECRET not given, exit."; exit 1; }
+[ "${USERNAME}" ] || { echo "USERNAME not given, exit."; exit 1; }
+[ "${CLIENT_ID}" ] || { echo "CLIENT_ID not given, exit."; exit 1; }
+[ "${CLIENT_SECRET}" ] || { echo "CLIENT_SECRET not given, exit."; exit 1; }
 
 # Write config.py config file from Docker env variables
 cat <<EOF > /opt/config.py
 root_path = "/opt/recordings"
-username = "$USERNAME"
-client_id = "$CLIENT_ID"
+username = "${USERNAME}"
+client_id = "${CLIENT_ID}"
 client_secret = "$CLIENT_SECRET"
 EOF
 
